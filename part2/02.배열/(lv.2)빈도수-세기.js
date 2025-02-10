17ecee89a71d8029b120ab6b690589db7cea26c4
smileeeeely <minjoole.ivy@gmail.com>
2025-02-10 10:02:00 +0900

    2/10

--- part2/02.배열/(lv.2)빈도수-세기.js
+++ part2/02.배열/(lv.2)빈도수-세기.js
@@ -12,29 +12,35 @@
 
 // TODO: 함수를 작성하세요.
 function getFrequency(arr) {
-  let cntArr = {};
-  arr.forEach((thing) => {
-    let check = false;
-    for (const it in cntArr) {
-      if (it === thing) {
-        check = true;
-        cntArr[it]++;
-        return;
-      }
+  const map = new Map();
+
+  arr.forEach((item) => {
+    let key;
+    if (typeof item === "object" && item !== null) {
+      key = Array.isArray(item)
+        ? `arr:${JSON.stringify(item)}`
+        : `obj:${JSON.stringify(item)}`;
+    } else if (typeof item === "number") {
+      key = Symbol(`num:${item}`);
+    } else {
+      key = item;
     }
-    if (!check) {
-        if(!isNaN(thing)){
-            console.log('thing',thing)
-            //왜 숫자인데 문자열로 들어감???
-            return;
-        }
-      cntArr[thing] = 1;
-    }
+    map.set(key, (map.get(key) || 0) + 1);
   });
-  console.log("cntArr", cntArr);
-  return cntArr;
+  const result = Object.create(null);
+  for (const [key, value] of map.entries()) {
+    if (typeof key === "symbol") {
+      result[Number(key.description.replace("num:", ""))] = value;
+    } else {
+      console.log('key',key)
+      result[key] = value;
+    }
+  }
+  return result;
 }
-getFrequency(["1", 1]);
+console.log(getFrequency(["1", 1]));
+console.log(getFrequency(["1", 1, true, false, true, [], {}, [], {}]));
+//아니면 걍 for문 돌리면서 숫자일 땐 숫자 내보내고 그러면 안되나?
 
 // export 를 수정하지 마세요.
 export { getFrequency };
